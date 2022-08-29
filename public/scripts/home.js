17bd2baf074e263d0e88b6a1763f680283d8a3ec
vagrant <bahar.hosseini@gmail.com>
2022-08-29 22:24:59 +0000

    Display each product element by creating appendProduct function

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,20 +1,40 @@
 // Client facing scripts here
 
+
+// A Function to create each product element
+const appendProduct = (productObj) =>{
+  const productTemplate = `
+  <main class="items-container">
+  <div class="item">
+    <h2>${productObj.name}</h2>
+    <img src="${productObj.photo_url}" />
+    <p>price: $${productObj.price}</p>
+    <details>
+      <p>${productObj.description}</p>
+    </details>
+  </div>
+</main>
+  `;
+  $('section').append(productTemplate);
+};
+
+
 // This script loads product info when the page loads
-$(document).ready( function () {
+$(document).ready(function() {
 
   // Load product information
   $.ajax({
     method: 'GET',
     url: '/api/products'
   })
-  .done((response) => {
-    const $productsList = $('#products');
-    $productsList.empty();
+    .done((response) => {
+      const $productsList = $('#products');
+      $productsList.empty();
 
-    for(const product of response.products) {
-      $(`<li class="product">`).text(product.name).appendTo($productsList);
-    }
-  });
+      console.log(response.products);
+      for (const product of response.products) {
+        appendProduct(product);
+      }
+    });
 
 });
