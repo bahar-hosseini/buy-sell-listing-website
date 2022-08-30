f34e82baa7a09e761b01589e54a44232c5d705bf
vagrant <bahar.hosseini@gmail.com>
2022-08-30 11:23:18 +0000

    moving post request for messages into messages.js

--- routes/post-messages-api.js
+++ routes/post-messages-api.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const router  = express.Router();
-const messageQueries = require('../db/queries/user_messages');
+// const express = require('express');
+// const router  = express.Router();
+// const messageQueries = require('../db/queries/user_messages');
 
-router.get('/', (req, res) => {
-  messageQueries.addMessage()
-    .then(messages => {
-      console.log('this is apiiiiiiiii',messages);
-      res.json({ messages });
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
-});
+// router.get('/', (req, res) => {
+//   messageQueries.addMessage()
+//     .then(messages => {
+//       console.log('this is apiiiiiiiii',messages);
+//       res.json({ messages });
+//     })
+//     .catch(err => {
+//       res
+//         .status(500)
+//         .json({ error: err.message });
+//     });
+// });
 
-module.exports = router;
+// module.exports = router;
