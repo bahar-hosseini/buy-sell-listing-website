8514599f0a21f2bb78500d6da8e5ddcf8f20c51b
Michael Wang <michaelwangxv@gmail.com>
2022-08-30 02:23:21 +0000

    Add product page

---
+++ db/queries/product.js
@@ -0,0 +1,11 @@
+const db = require('../connection');
+
+// Query the information for a single product based on its ID
+const getProduct = (productId) => {
+  return db.query(`SELECT * FROM products WHERE id = ${productId};`)
+    .then(data => {
+      return data.rows;
+    });
+};
+
+module.exports = { getProduct };
