bc058fd2fa8c6da6e5cefbaa3256c2405803abb3
vagrant <bahar.hosseini@gmail.com>
2022-08-31 06:04:52 +0000

    fix a query bug

--- routes/products-api.js
+++ routes/products-api.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router  = express.Router();
 const productQueries = require('../db/queries/products');
+const messageQueries = require('../db/queries/user_messages');
 
 router.get('/', (req, res) => {
   productQueries.getProducts()
+
     .then(products => {
       res.json({ products });
     })
@@ -15,15 +17,16 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  productQueries.addProduct(req.body)
-   .then(data => {
-    console.log(data);
-   // alert ("Product added!")
-    res.redirect('/home')
+  productQueries.addProduct(req.body);
+  messageQueries.getProductMsg(req.body)
+    .then(data => {
+      console.log(data);
+      // alert ("Product added!")
+      res.redirect('/home');
     //res.json({data});
-   })
-  console.log(req.body)
+    });
+  console.log(req.body);
   //res.render('home')
-})
+});
 
 module.exports = router;
