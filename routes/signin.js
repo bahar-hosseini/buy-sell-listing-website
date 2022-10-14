b5c265dd2f3200a434cdfcadeb80dbf8ec5cb42d
vagrant <bahar.hosseini@gmail.com>
2022-10-14 13:49:00 +0000

    Add README.md

--- routes/signin.js
+++ routes/signin.js
@@ -1,36 +1,36 @@
-const express = require('express');
-const router  = express.Router();
-const session =  require('express-session');
+const express = require("express");
+const router = express.Router();
+const session = require("express-session");
 
-express().use(session({ secret: 'lighthouselab', resave: false, saveUninitialized: true }));
+express().use(
+  session({ secret: "lighthouselab", resave: false, saveUninitialized: true })
+);
 
-const queryUser = require('../db/queries/user_login');
+const queryUser = require("../db/queries/user_login");
 
-router.get('/', (req, res) => {
-  res.render('signin');
+router.get("/", (req, res) => {
+  res.render("signin");
 });
 
 //Post Login
-router.post('/', (req,res) =>{
-  const {email, password} = req.body;
+router.post("/", (req, res) => {
+  const { email, password } = req.body;
 
-  queryUser.getUser(email)
-  //for now Just works with our db data!!
-    .then(response => {
-      req.session['user_id'] = response.id;
+  queryUser
+    .getUser(email)
+    //for now Just works with our db data!!
+    .then((response) => {
+      req.session["user_id"] = response.id;
       if (response.is_admin) {
-        req.session['authorized'] = true;
-        res.redirect('/home');
+        req.session["authorized"] = true;
+        res.redirect("/home");
       } else {
-        req.session['authorized'] = false;
-        res.redirect('/home');
+        req.session["authorized"] = false;
+        res.redirect("/home");
       }
-
     })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
     });
 });
 
